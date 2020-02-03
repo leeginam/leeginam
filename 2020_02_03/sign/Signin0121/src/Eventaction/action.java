43a39c5745a8e8825ce5d206ce585f5e0ebdac42
leeginam <58802017+leeginam@users.noreply.github.com>
2020-02-03 16:05:19 +0900

    올렸씁니다

--- 2020_02_03/sign/Signin0121/src/Eventaction/action.java
+++
@@ -1,9 +0,0 @@
-package Eventaction;
-
-import javax.servlet.http.*;
-
-import Dto.ActionForward;
-
-public interface action {
-	public ActionForward execute(HttpServletRequest request,HttpServletResponse response) throws Exception;
-}
